99b20cc364432731fb223b4f03c30c350d774f71
Chisom Daniel <chinwezechisom@gmail.com>
2023-02-12 13:54:19 +0100

    added doc strings

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-"""The base class for the airbnb project"""
+"""The base class for the airbnb project
+
+This file contains the BaseModel class for the project
+"""
 import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel():
+    '''The BaseModel class that defines all common
+    attributes/methods for other classes
+
+    Attributes:
+        id (str): string - assign with an uuid when an instance is created
+            the goal is to have unique id for each BaseModel
+        created_at: datetime - assign with the current datetime when
+            an instance is created
+        updated_at: datetime - assign with the current datetime when
+            an instance is created and it will be updated every time
+            you change your object
+        **kwargs (obj: `dict`, optional): a dict with attributes from
+            another obj of the same class can be provided to recreate the class
+    '''
 
     def __init__(self, *args, **kwargs):
+        '''The init method for the class'''
         
         if len(kwargs) > 3:
             self.id = kwargs['id']
@@ -20,15 +38,23 @@ class BaseModel():
             models.storage.new(self)
 
     def __str__(self):
+        '''The string representation for the object when it is printed'''
         string = "[{}] ({}) {}".format(self.__class__.__name__,
                                        self.id, self.__dict__)
         return string
 
     def save(self):
+        '''saves the model to a file (database) and update the
+        `updated_at attribute`
+        '''
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        '''Creates a dictionary of all the attributes in the object
+        
+        Returns the dictionary
+        '''
         my_dict = {}
         for key, value in self.__dict__.items():
             my_dict[key] = value
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,17 +8,32 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
+    '''Manages the database for our objects
+
+    Private class attribute:
+        __file_path (str): string - path to the JSON file (ex: file.json)
+        __objects: dictionary - empty but will store all objects by
+            <class name>.id (ex: to store a BaseModel object with
+            id=12121212, the key will be BaseModel.12121212)
+    '''
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
+        '''returns the dictionary __objects'''
         return self.__objects
 
     def new(self, obj):
+        '''sets in `__objects` the `obj` with <obj class name>.id
+        
+        Args:
+        obj (obj of a valid class): an object of one of our classes
+        '''
         string = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[string] = obj
 
     def save(self):
+        '''serializes __objects to the JSON file (path: __file_path)'''
         new_dict = {}
         for key, value in self.__objects.items():
             new_dict[key] = value.to_dict()
@@ -27,6 +42,10 @@ class FileStorage:
             json.dump(new_dict, fd)
 
     def reload(self):
+        '''deserializes the JSON file to __objects (only if the
+        JSON file (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        '''
 
         try:
             with open(self.__file_path, 'r') as fd:
